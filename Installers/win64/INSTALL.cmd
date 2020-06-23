a9b5c46322bf435cb99e712e6770f8c15db5ba2c
papa <papa@DESKTOP-A221D85>
2020-06-23 13:06:08 +0200

    Added the install cmd

--- Installers/win64/INSTALL.cmd
+++ Installers/win64/INSTALL.cmd
@@ -1,10 +1,15 @@
-tar -xf %cd%\pgsql.zip
+SET mypath=%~dp0
+rd /S /Q "%mypath%pgsql"
 
-nssm install TimeControl %cd%\Monitor.exe
-nssm set TimeControl AppDirectory %cd%
-nssm set TimeControl DisplayName TimeControl
-nssm set TimeControl Start SERVICE_AUTO_START
-nssm set TimeControl Type SERVICE_INTERACTIVE_PROCESS
-nssm set TimeControl ObjectName LocalSystem
+tar -xf "%mypath%pgsql.zip" -C "%mypath% "
+
+icacls %mypath% /grant SYSTEM:(OI)(CI)F /T
+
+"%mypath%\nssm" install TimeControl "%mypath%Monitor.exe"
+"%mypath%\nssm" set TimeControl AppDirectory "%mypath% "
+"%mypath%\nssm" set TimeControl DisplayName TimeControl
+"%mypath%\nssm" set TimeControl Start SERVICE_AUTO_START
+"%mypath%\nssm" set TimeControl Type SERVICE_INTERACTIVE_PROCESS
+"%mypath%\nssm" set TimeControl ObjectName LocalSystem
 
 Pause TimeControl installed. Press a key ...
