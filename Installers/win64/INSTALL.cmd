ba2c3508aaa1736d60857f9000d40263a88554d1
aintitfun <opolofdez@gmail.com>
2022-11-25 18:05:00 +0100

    changed the installer to set the pg password on the install

--- Installers/win64/INSTALL.cmd
+++ Installers/win64/INSTALL.cmd
@@ -5,16 +5,19 @@ tar -xf "%mypath%pgsql.zip" -C "%mypath% "
 
 icacls %mypath% /grant SYSTEM:(OI)(CI)F /T
 
+pgsql\bin\pg_ctl start -D pgsql\data 
+set /p password=Input the password: 
+pgsql\bin\psql -U postgres -d monitor -c "alter user postgres with password '%password%'" 
+pgsql\bin\pg_ctl stop -D pgsql\data 
+powershell -Command "(gc pgsql\data\pg_hba.conf) -replace 'trust', 'md5' | Out-File -encoding ASCII pgsql\data\pg_hba.conf"
+
 "%mypath%\nssm" remove TimeControl"
 
-"%mypath%\nssm" install TimeControl "%mypath%BlazorServerApp.exe"
+"%mypath%\nssm" install TimeControl "%mypath%Monitor.exe"
 "%mypath%\nssm" set TimeControl AppDirectory "%mypath% "
 "%mypath%\nssm" set TimeControl DisplayName TimeControl
 "%mypath%\nssm" set TimeControl Start SERVICE_AUTO_START
 "%mypath%\nssm" set TimeControl Type SERVICE_INTERACTIVE_PROCESS
 "%mypath%\nssm" set TimeControl ObjectName LocalSystem
-"%mypath%\nssm" set TimeControl AppRestartDelay 7500
-
-aspnetcore-runtime-3.0.0-win-x64.exe
 
 Pause TimeControl installed. Press a key ...
